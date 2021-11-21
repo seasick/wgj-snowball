5dd257019815376ec476778decd9aed4c1b86ca0
Clemens Solar <solar.c@gmx.net>
2021-11-21 17:43:18 +0100

    feat: add basic menu screen

---
+++ scenes/MainMenu/MainMenu.gd
@@ -0,0 +1,32 @@
+extends MarginContainer
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	var export_config: ConfigFile = ConfigFile.new()
+	var err = export_config.load("res://export_presets.cfg")
+	var version = "dev"
+	
+	var newGameLabel = $VBoxContainer/VBoxContainer/NewGameLabel
+	var optionsLabel = $VBoxContainer/VBoxContainer/OptionsLabel
+	var versionLabel = $VBoxContainer/VersionLabel
+	
+	# Display version number (found in first preset of export config)
+	if err == OK:
+		version = export_config.get_value("preset.0.options", "application/file_version")
+		versionLabel.text = version
+	else:
+		print('[engine_root] Error open export_presets.cfgs')
+		
+	if newGameLabel.connect("button_down", self, "_on_NewGameLabel_button_down") != OK:
+		print("Error connecting to newGame label")
+		
+	if optionsLabel.connect("button_down", self, "_on_OptionsLabel_button_down") != OK:
+		print("Error connecting to options label")
+
+func _on_NewGameLabel_button_down():
+	get_tree().change_scene("res://scenes/Playground.tscn")
+
+func _on_OptionsLabel_button_down():
+	# TODO
+	print("NOT IMPLEMENTED")
